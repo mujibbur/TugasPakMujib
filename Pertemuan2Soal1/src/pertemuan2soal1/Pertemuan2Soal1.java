7126a7e7020b9f9c0da2e6dfe13b8deef705065f
mujibbur <mujibbur.rohman@gmail.com>
2024-08-11 09:41:00 +0700

    Tugas Pertemuan 2 Soal 1

---
+++ Pertemuan2Soal1/src/pertemuan2soal1/Pertemuan2Soal1.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package pertemuan2soal1;
+
+/**
+ *
+ * @author gipsy linux
+ */
+public class Pertemuan2Soal1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int a = 5;
+        int b = 10;
+        int c = a + b;
+        float d=c;
+        System.out.println(c);
+        System.out.println(d);
+    }
+
+}
